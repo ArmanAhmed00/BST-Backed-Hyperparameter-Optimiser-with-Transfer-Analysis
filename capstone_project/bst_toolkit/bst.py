128aab07dfc00de0588d7b488825a1f8b18172d4
Arman Ahmed <89986549+ArmanAhmed00@users.noreply.github.com>
2026-05-10 04:21:40 +0200

    BST Introspection

--- capstone_project/bst_toolkit/bst.py
+++ capstone_project/bst_toolkit/bst.py
@@ -157,3 +157,27 @@ class BST:
             self._postorder(node.left, result)
             self._postorder(node.right, result)
             result.append(node)
+
+
+    def get_level_order(self) -> List[List[TrialNode]]:
+        """Returns a list of levels (BFS), where each level is a list of nodes."""
+        if not self.root:
+            return []
+        
+        levels = []
+        queue = [self.root]
+        
+        while queue:
+            level_size = len(queue)
+            current_level = []
+            for _ in range(level_size):
+                # Pop the first element (Queue behavior)
+                node = queue.pop(0)
+                current_level.append(node)
+                
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            levels.append(current_level)
+        return levels
